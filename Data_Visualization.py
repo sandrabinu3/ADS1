6033e5e77c1335c21a780583cd5bca62f2dfc970
sandrabinu3 <sandrabinu99@gmail.com>
2023-11-07 11:04:45 +0000

    renamed

---
+++ Data_Visualization.py
@@ -0,0 +1,10 @@
+#Data_Visualization
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+cancer=pd.read_csv(r"C:\Users\sandr\Documents\Assignment\Dataset\total-cancer-deaths-by-type.csv")
+
+print(cancer.columns)
+
